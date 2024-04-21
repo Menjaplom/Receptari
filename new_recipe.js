9dd8eb55ab64ab7d5fad06109d43af03f00ec38c
Menjaplom <correuperamerda@hotmail.com>
2024-04-21 23:19:23 +0200

    Start popullating new create recipe

--- new_recipe.js
+++ new_recipe.js
@@ -1,15 +1,40 @@
+
+function removeUserListElem(obj) {
+    obj.parentElement.remove(); // Remove the parent li element
+}
+
+
+const title = document.getElementById("title");
+const comp_title = document.getElementById("componentTitle");
+
+function syncComponentName() {
+    comp_title.textContent = title.value;
+}
+
+
+const ingr = document.getElementById("ingredients");
+
 function addIngredient() {
-    const elem = document.getElementById("ingredients");
-    elem.insertAdjacentHTML("beforeend", '<li>' +
+    ingr.insertAdjacentHTML("beforeend", '<li>' +
         '<input type="text" name="ingredient">' +
-        '<button>X</button>' +
+        '<button onclick="removeUserListElem(this)">X</button>' +
         '</li>');
 }
 
-function removeIngredient(ing_id) {
-    const elem = document.getElementById("ingredients");
-    elem.insertAdjacentHTML("beforeend", '<li>' +
-        '<input type="text" name="ingredient">' +
-        '<button>X</button>' +
-        '</li>');
+
+const total_ingr = document.getElementById("ingredients_total");
+const comp_sel = document.getElementById("components_selector");
+
+function showSimpleLayout() {
+    hideComplexRecipeLayout();
+}
+
+function showComplexLayout() {
+    total_ingr.style.display = "";
+    comp_sel.style.display = "";
+}
+
+function hideComplexRecipeLayout() {
+    total_ingr.style.display = "none";
+    comp_sel.style.display = "none";
 }
