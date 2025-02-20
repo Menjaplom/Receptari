b4c1f15f90cfea5ed6e4154b03641407e040f8db
Menjaplom <albertcanyelles@gmail.com>
2025-02-20 20:21:02 +0100

    Add  db table creation

--- src/services/database/sqlite/database.ts
+++ src/services/database/sqlite/database.ts
@@ -1,7 +1,10 @@
-import { dbNameLit, dbURLLit } from '@/literals'
-import * as dbLit from '@/services/database/sqlite/db_literals'
+import { dbURLLit } from '@/literals'
+import * as createLit from '@/services/database/sqlite/creationLiterals'
 import { type dbConnection } from '../dbInterface'
 import initSqlJs, { type Database } from 'sql.js'
+import type NewRecipe from '@/types/NewRecipe'
+import * as sharedLit from './sharedLiterals'
+import * as insertLit from './insertionLiterals'
 
 export class DBSqlite implements dbConnection {
   ready: boolean
@@ -12,46 +15,83 @@ export class DBSqlite implements dbConnection {
   }
 
   async connect(dbName: string): Promise<void> {
-    const sqlPromise = initSqlJs()
-    const fetchPromise = fetch(dbURLLit)
+    const sqlPromise = initSqlJs({ locateFile: (_) => '/node_modules/sql.js/dist/sql-wasm.wasm' })
+    console.log('Wasm promised')
+    let fetchGoing = true
+    
+    let fetchPromise
+    try {
+      fetchPromise = fetch(dbURLLit).then(r => {
+        console.log('im accepting the goddamn promise')
+        if(JSON.stringify(r) == '{}') {
+          return { response: undefined, going: false }
+        }
+        return { response: r, going: true }
+      }).catch((error) => {
+        console.log(error)
+        return { response: undefined, going: false }
+      })
+    } catch {
+      fetchPromise = { response: undefined, going: false }
+    }
     const [SQL, fetchResponse] = await Promise.all([sqlPromise, fetchPromise])
-    if (!fetchResponse.ok) {
+    console.log('Wasm loaded')
+    console.log(`fetchGoing ${fetchResponse.going}`)
+    console.log(`fetchResponse ${fetchResponse.response?.ok}`)
+    if (!fetchGoing || !fetchResponse.response?.ok) {
       this.db = new SQL.Database()
-      this.db.run(dbLit.createTableRecipes)
+      
+      console.log(createLit.createTableRecipes)
+      this.db.run(createLit.createTableRecipes)
       console.log('Table Recipes created.')
-      this.db.run(dbLit.createTableRecipeMedia)
+      this.db.run(createLit.createTableRecipeMedia)
       console.log('Table RecipeMedia created.')
-      this.db.run(dbLit.createTableCategories)
+      this.db.run(createLit.createTableCategories)
       console.log('Table Categories created.')
-      this.db.run(dbLit.createTableRecipeCategory)
+      this.db.run(createLit.createTableRecipeCategory)
       console.log('Table RecipeCategory created.')
-      this.db.run(dbLit.createTableTags)
+      this.db.run(createLit.createTableTags)
       console.log('Table TableTags created.')
-      this.db.run(dbLit.createTableRecipeTags)
+      this.db.run(createLit.createTableRecipeTags)
       console.log('Table RecipeTags created.')
-      this.db.run(dbLit.createTableTools)
+      this.db.run(createLit.createTableTools)
       console.log('Table Tools created.')
-      this.db.run(dbLit.createTableRecipeTools)
+      this.db.run(createLit.createTableRecipeTools)
       console.log('Table RecipeTools created.')
-      this.db.run(dbLit.createTableIngredients)
+      this.db.run(createLit.createTableIngredients)
       console.log('Table Ingredients created.')
-      this.db.run(dbLit.createTableRecipeIngredients)
+      this.db.run(createLit.createTableRecipeIngredients)
       console.log('Table RecipeIngredients created.')
-      this.db.run(dbLit.createTableDirections)
+      this.db.run(createLit.createTableDirections)
       console.log('Table Directions created.')
-      this.db.run(dbLit.createTableRecipeDirections)
+      this.db.run(createLit.createTableRecipeDirections)
       console.log('Table RecipeDirections created.')
-      this.db.run(dbLit.createTableDirectionMedia)
+      this.db.run(createLit.createTableDirectionMedia)
       console.log('Table DirectionMedia created.')
-      this.db.run(dbLit.createTableRecipeComponents)
+      this.db.run(createLit.createTableRecipeComponents)
       console.log('Table RecipeComponents created.')
       this.ready = true
     } else {
-      const dbArr = await fetchResponse.arrayBuffer().then((arr) => new Uint8Array(arr))
+      const dbArr = await fetchResponse.response.arrayBuffer().then((arr) => new Uint8Array(arr))
       this.db = new SQL.Database(dbArr)
       this.ready = true
     }
 
     return Promise.resolve()
   }
+
+  async addRecipe(recipe: NewRecipe): Promise<void> {
+    if (!this.ready) return Promise.reject('DB not ready')
+    this.db!.run(sharedLit.beginTransaction)
+    const stmt = this.db!.prepare(insertLit.insertRecipe);
+    let stmtObject = {}
+    
+    /*const result = stmt.getAsObject({':name': recipe.title,
+      ':yield': recipe.recipeYield,
+      ':prepTime': recipe.prepTime,
+      ':cookTime': recipe.cookTime,
+      ':difficulty': recipe.difficulty})*/
+    //return Promise.reject('DB not ready')
+  }
+ 
 }
---
+++ src/services/database/sqlite/sharedLiterals.ts
@@ -0,0 +1,20 @@
+export const enableForeingKeys = `PRAGMA foreign_keys = ON`
+export const beginTransaction = `BEGIN TRANSACTION`
+export const commitTransaction = `COMMIT`
+export const rollbackTransaction = `ROLLBACK`
+//export const lastIdInsert = `SELECT LAST_INSERT_ID() AS id` // TODO: Check if it is necessary the "AS id" part
+
+export const recipes = `Recipes`
+export const recipeMedia = `RecipeMedia`
+export const categories = `Categories`
+export const recipeCategory = `RecipeCategory`
+export const tags = `Tags`
+export const recipeTags = `RecipeTags`
+export const tools = `Tools`
+export const recipeTools = `RecipeTools`
+export const ingredients = `Ingredients`
+export const recipeIngredients = `RecipeIngredients`
+export const directions = `Directions`
+export const recipeDirections = `RecipeDirections`
+export const directionMedia = `DirectionMedia`
+export const recipeComponents = `RecipeComponents`
